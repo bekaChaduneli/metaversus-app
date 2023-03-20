310c9c4197a7ca550d7ce0d4c8559ddc369e0620
beka chaduneli <beka@kali.ge>
2023-03-20 20:12:54 +0400

    format document

--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -1,40 +1,40 @@
-"use client";
+'use client';
 
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 
-import styles from "../styles";
-import { staggerContainer, fadeIn, planetVariants } from "../utils/motion";
-import { newFeatures } from "../constants";
-import { NewFeatures, TitleText, TypingText } from "../components";
+import styles from '../styles';
+import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
+import { newFeatures } from '../constants';
+import { NewFeatures, TitleText, TypingText } from '../components';
 
 const WhatsNew = () => (
   <section className={`${styles.paddings} relative z-10`}>
     <motion.div
       variants={staggerContainer}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: "false", amount: 0.25 }}
+      initial='hidden'
+      whileInView='show'
+      viewport={{ once: 'false', amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
     >
       <motion.div
-        variants={fadeIn("left", "tween", 0.2, 1)}
-        className="flex-[0.75] flex justify-center flex-col "
+        variants={fadeIn('left', 'tween', 0.2, 1)}
+        className='flex-[0.75] flex justify-center flex-col '
       >
-        <TypingText title="| Whats's New" />
+        <TypingText title='| Whats New' />
         <TitleText title={<>Whats's new about Metaversus</>} />
-        <div className="mt-[48px] flex flex-wrap justify-between gap-[24px]">
+        <div className='mt-[48px] flex flex-wrap justify-between gap-[24px]'>
           {newFeatures.map((feature, index) => (
             <NewFeatures key={feature.title} {...feature} />
           ))}
         </div>
       </motion.div>
       <motion.div
-        variants={planetVariants("right")}
+        variants={planetVariants('right')}
         className={`flex-1 ${styles.flexCenter}`}
       >
         <img
-          src="/whats-new.png"
-          alt="get-started"
+          src='/whats-new.png'
+          alt='get-started'
           className={`flex-1 ${styles.flexCenter}`}
         />
       </motion.div>
